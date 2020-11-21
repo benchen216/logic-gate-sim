d96f0650ee59f973f761100751cf4fe22946d7d6
ben <benchen216@gmail.com>
2020-11-21 22:11:59 +0800

    add pybind11

--- build_gate.cpp
+++ build_gate.cpp
@@ -1,4 +1,11 @@
 //
 // Created by ben on 11/19/20.
 //
+#include <iostream>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
 
+PYBIND11_MODULE(build_gate, m){
+
+}
