8b76b5d4e262779b7382f3d46e0a554414d53141
ben chen <benchen216@gmail.com>
2020-12-20 14:56:55 +0800

    web api post file tested

--- web.py
+++ web.py
@@ -1,5 +1,36 @@
-from flask import Flask,  render_template
+from flask import Flask,  render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = './tmp'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif', 'txt', '.csv'])
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                   filename))
+            return "uploaded"
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 @app.route('/')
